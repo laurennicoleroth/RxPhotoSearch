85512277debbc42f06740c3a9cc1706d1697b449
Lauren N. Roth <laurennicoleroth@gmail.com>
2017-09-06 19:22:20 -0400

    Add init from json dict and method for large photo url

--- IVYRxPhotos/Photo.swift
+++ IVYRxPhotos/Photo.swift
@@ -15,9 +15,23 @@ struct Photo {
   let photoID: String?
   let secret: String?
   let server: String?
+  let title: String?
+  
+  init(jsonDictionary : JSON) {
+    self.title = jsonDictionary["title"].stringValue
+    self.farm = jsonDictionary["farm"].stringValue
+    self.ownerID = jsonDictionary["owner"].stringValue
+    self.photoID = jsonDictionary["id"].stringValue
+    self.secret = jsonDictionary["secret"].stringValue
+    self.server = jsonDictionary["server"].stringValue
+  }
   
   func getPhotoThumbnailURL() -> String {
     return "https://farm\(farm!).staticflickr.com/\(server!)/\(photoID!)_\(secret!)_m.jpg"
+  }
+  
+  func getLargePhotoURL() -> String {
+    return "https://farm\(farm!).staticflickr.com/\(server!)/\(photoID!)_\(secret!)_b.jpg"
   }
   
   
